5ef638cd4e93486ed3ac351d70e4f12afc2715eb
ryan <ryanperryj@gmail.com>
2021-12-07 22:42:20 -0500

    Fixed dependencies

--- World/World.gd
+++ World/World.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+func _ready():
+	var sz_ch = 3
+	var tl_types = []
+	var x = 0
+	var y = 0
+	while x < sz_ch:
+		tl_types.append([])
+		y = 0
+		while y < sz_ch:
+			tl_types[x].append(-1)
+			y += 1
+		x += 1
+	
+	print(tl_types)
+
 func _process(delta):
 	if Input.is_action_just_pressed("reload"):
 		get_tree().reload_current_scene()
